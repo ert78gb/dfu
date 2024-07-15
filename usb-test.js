49a0c48ed09f82cb1e232bdcee819b9023896388
Kiss Róbert <ert78gb@gmail.com>
2024-07-15 21:35:30 +0200

    use webusb

--- usb-test.js
+++ usb-test.js
@@ -1,5 +1,43 @@
-import { getDeviceList } from "usb";
+import { WebUSB } from 'usb'
+import { inspect } from 'node:util'
 
-const devices = getDeviceList()
+const DFU_REQUESTS = Object.freeze({
+  DFU_DETACH: 0,
+  DFU_DNLOAD: 1,
+  DFU_UPLOAD: 2,
+  DFU_GETSTATUS: 3,
+  DFU_CLRSTATUS: 4,
+  DFU_GETSTATE: 5,
+  DFU_ABORT: 6
+})
 
-console.log(devices.filter(device => device.deviceDescriptor.idVendor === 7504))
+const customWebUSB = new WebUSB({
+  allowAllDevices: true
+})
+
+const devices = await customWebUSB.getDevices()
+
+const device = devices.find(webUsbDevice => {
+  const { idVendor, bcdUSB } = webUsbDevice.device.deviceDescriptor
+
+  return idVendor === 7504 && bcdUSB === 0x200
+})
+
+console.log(inspect(device, { depth: 20 }))
+
+await device.open()
+
+const response = await device.controlTransferIn(
+  {
+    index: 1,
+    requestType: 'class',
+    recipient: 'interface',
+    request: DFU_REQUESTS.DFU_DNLOAD,
+    value: 0
+  },
+  1
+)
+
+console.log(inspect(response, {depth: 20}))
+
+await device.close()
