187353db00abfbdafafcad4f498964a0b5eab8ad
Kiss Róbert <ert78gb@gmail.com>
2024-08-06 14:45:08 +0200

    open serial port

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
 import { SerialPort } from 'serialport'
 
 
-console.log(await SerialPort.list())
+const devices = await SerialPort.list()
+
+const device = devices.find(x => x.vendorId?.toLowerCase() === '1d50' && x.productId?.toLowerCase() === '6125')
+
+if (!device) {
+  console.error("Can't find device")
+  process.exit(1)
+}
+console.log(device)
+const serialPort = new SerialPort({
+  path: device.path,
+  baudRate: 115200,
+  autoOpen: false,
+})
+
+serialPort.open((err) => {
+  if (err) {
+    return console.log('Error opening port: ', err.message)
+  }
+
+  console.log('port opened')
+
+  serialPort.close((err) => {
+    if (err) {
+      return console.log('Error closing port: ', err.message)
+    }
+  })
+})
